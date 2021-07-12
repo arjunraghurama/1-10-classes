fbb3f31b85875f36d3cb80730abc683a1e1acec2
arjunraghurama <arjunr0019@gmail.com>
2021-07-12 08:49:42 +0530

    Added local caching

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import json
 import requests
 import SessionState
+from datetime import datetime
 
 st.set_page_config(
         page_title="My Class",
@@ -15,9 +16,24 @@ add_selectbox = st.sidebar.selectbox(
     index=1
 )
 
-response = requests.get("https://raw.githubusercontent.com/arjunraghurama/1-10-classes/main/data/data.json")
-data = json.loads(response.text)
-video_list = data[add_selectbox]
+# To cache the data for 1 Day
+session_state = SessionState.get(data=None,last_updated=None,days=0)
+
+now = datetime.now()
+if session_state.last_updated is not None:
+    lastUpdate = session_state.last_updated
+    session_state.days = (now -lastUpdate).days
+
+if session_state.last_updated is None or session_state.days >=1:
+    response = requests.get("https://raw.githubusercontent.com/arjunraghurama/1-10-classes/main/data/data.json")
+    url_data = json.loads(response.text)
+    session_state.data = url_data
+    session_state.last_updated = now
+        
+# response = requests.get("https://raw.githubusercontent.com/arjunraghurama/1-10-classes/main/data/data.json")
+# data = json.loads(response.text)
+
+video_list = session_state.data[add_selectbox]
 for i in range(len(video_list)):
     st.header(str(1+i)+ ". " + video_list[i][1])
     st.video(video_list[i][2])
