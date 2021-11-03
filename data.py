46ffd95d93468c227f250879dbae9e0d8c9057eb
arjunraghurama <arjunr0019@gmail.com>
2021-11-03 12:05:40 +0530

    Update data.py

--- data.py
+++ data.py
@@ -81,7 +81,7 @@ while pageToken:
 
 dataset.reverse()
 
-class_list = ["Classes 1-3", "4th Class", "5th Class", "6th Class", "7th Class", "8th Class", "9th Class", "10th Class"]
+class_list = ["Classes 1-3", "1-3Class", "4th Class", "5th Class", "6th Class", "7th Class", "8th Class", "9th Class", "10th Class"]
 
 res = defaultdict(list)
 for entry in dataset:
